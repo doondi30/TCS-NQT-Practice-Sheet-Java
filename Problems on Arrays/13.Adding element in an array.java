efa6c1f590372c22f8361bc72e54e247c0d9f475
R DOONDI GANGADHAR <160998038+doondi30@users.noreply.github.com>
2026-01-15 22:30:47 +0530

    Create 13.Adding element in an array.java

---
+++ Problems on Arrays/13.Adding element in an array.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5};
+
+        ArrayList <Integer> ali = new ArrayList<>();
+
+        for (int i : arr)
+            ali.add(i);
+
+        // insert at beginning
+        ali.add(0, 0);
+
+        // insert at end
+        ali.add(9);
+
+        // insert at position (index 3)
+        ali.add(3, 21);
+
+        for (int i : ali)
+            System.out.println(i);
+    }
+}
