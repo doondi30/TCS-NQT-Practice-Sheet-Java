6792c11bd387fdba5cfe3d82ead561b669f44147
R DOONDI GANGADHAR <160998038+doondi30@users.noreply.github.com>
2026-01-22 01:51:39 +0530

    commit done
    Updated the input array and modified the logic to store ranks in a separate result array instead of modifying the original array directly.

--- Problems on Arrays/18.Replace elements by its rank in the array.java
+++ Problems on Arrays/18.Replace elements by its rank in the array.java
@@ -3,26 +3,29 @@ import java.util.*;
 class Main {
     public static void main(String[] args) {
 
-        int[] arr = {4, 3, 2, 10};
-        int[] temp = arr.clone();   
+        int[] arr = {3, 2, 1, 0};
+        int[] temp = arr.clone();
         Arrays.sort(temp);
 
         int len = arr.length;
         int rank = 1;
+        int[] result = new int[len]; 
 
         // Replace elements by rank
+
         for (int i = 0; i < len; i++) {
-            if (i > 0 && temp[i] != temp[i - 1])
+            if (i > 0 && temp[i] != temp[i - 1]) {
                 rank++;
+            }
 
             for (int j = 0; j < len; j++) {
                 if (arr[j] == temp[i]) {
-                    arr[j] = rank;
+                    result[j] = rank;
                 }
             }
         }
 
-        for (int ele : arr)
+        for (int ele : result)
             System.out.print(ele + " ");
     }
 }
