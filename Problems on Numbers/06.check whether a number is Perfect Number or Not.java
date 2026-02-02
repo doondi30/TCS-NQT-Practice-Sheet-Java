9c94aa6753f560f5dee12381abe27adc0eca897e
R DOONDI GANGADHAR <160998038+doondi30@users.noreply.github.com>
2026-02-02 23:55:18 +0530

    Add perfect number check in Main class
    Implement a program to check if a number is perfect.

--- Problems on Numbers/06.check whether a number is Perfect Number or Not.java
+++ Problems on Numbers/06.check whether a number is Perfect Number or Not.java
@@ -1,1 +1,29 @@
+class Main {
+    public static void main(String[] args){
+        int n=28;
+        int ans=1;
+      
+         if (n <= 1) {
+            System.out.print(false);
+            return;
+        }
+      
+        for(int i=2;i<=n/2;i++){
+            if(n%i==0){
+                ans+=i;
+            }
+        }
+        System.out.print(n==ans);
+    }
+}
 
+
+
+// for(int i = 2; i * i <= n; i++){
+//             if(n % i == 0){
+//                 ans += i;
+//                 if(i != n / i){
+//                     ans += n / i;
+//                 }
+//             }
+//         }
