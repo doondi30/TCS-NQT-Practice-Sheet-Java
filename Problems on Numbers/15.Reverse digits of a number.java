25a488dc454764d476d82d395503c3f31b2df102
R DOONDI GANGADHAR <160998038+doondi30@users.noreply.github.com>
2026-02-11 23:55:08 +0530

    Implement digit reversal in Main class

--- Problems on Numbers/15.Reverse digits of a number.java
+++ Problems on Numbers/15.Reverse digits of a number.java
@@ -1,1 +1,15 @@
-sample
+class Main {
+    public static void main(String[] args) {
+        int num = 12345;
+        int n = num;
+        int ans = 0;
+
+        while (n > 0) {
+            ans = ans * 10 + (n % 10);
+            n = n / 10;
+        }
+
+        System.out.println("Original number: " + num);
+        System.out.println("Reversed number: " + ans);
+    }
+}
