be20e659d57d9f298d830ea798f3f0e5aa93902b
R DOONDI GANGADHAR <160998038+doondi30@users.noreply.github.com>
2026-02-15 01:36:06 +0530

    Implement power calculation using a loop

---
+++ Problems on Numbers/19.Power of a Number.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        int ans=1,x=21,n=2;
+        for(int i=0;i<n;i++){
+            ans*=x;
+        }
+        System.out.println(ans);
+    // System.out.println( (int) Math.pow(x,n) );
+    }
+}
